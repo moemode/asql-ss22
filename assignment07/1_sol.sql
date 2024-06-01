102e6e9890c465ab04cd1db38aa8736e88f1b6d1
moemode <pfeffer.moritz@gmail.com>
2024-06-01 12:14:09 +0200

    Solve assignment 7.1 c)

--- assignment07/1_sol.sql
+++ assignment07/1_sol.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS puzzle_input;
 
+-- (a)
 CREATE TABLE puzzle_input (
     line int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     val int
@@ -7,6 +8,7 @@ CREATE TABLE puzzle_input (
 
 \copy puzzle_input(val) FROM 'assignment07/1_input';
 
+-- (b)
 WITH inc AS (
     SELECT (LAG(p.val, 1) OVER (ORDER BY line) < p.val) AS "inc?"
     FROM puzzle_input as p
@@ -14,3 +16,25 @@ WITH inc AS (
 SELECT COUNT(*) AS n_depth_increases
 FROM inc
 WHERE inc."inc?";
+
+
+-- (c)
+\set wsize 3
+
+WITH window_sums(line, sum) AS (
+    SELECT s.line,
+    SUM(s.val) OVER win
+    FROM puzzle_input AS s
+    WINDOW win AS (ORDER BY s.line ROWS BETWEEN CURRENT ROW AND (:wsize - 1) FOLLOWING)
+    ORDER BY s.line
+    LIMIT  (SELECT COUNT(*) FROM puzzle_input) - (:wsize - 1)
+),
+inc AS (
+    SELECT (LAG(w.sum, 1) OVER (ORDER BY w.line) < w.sum) AS "inc?"
+    FROM window_sums as w
+)
+SELECT COUNT(*) AS n_depth_increases
+FROM inc
+WHERE inc."inc?";
+
+
